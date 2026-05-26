20439834307de579882531175d05927897bf6633
Boris Truong <linhtch90@gmail.com>
2026-05-27 00:03:53 +0700

    [Leetcode-55] - Jump Game - Greedy - Medium

---
+++ MyLeetcodeCSharp/Leetcode55JumpGame.cs
@@ -0,0 +1,18 @@
+namespace MyLeetcodeCSharp;
+
+public class Leetcode55JumpGame
+{
+    public bool CanJump(int[] nums)
+    {
+        int goal = nums.Length - 1;
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            if (i + nums[i] >= goal)
+            {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}

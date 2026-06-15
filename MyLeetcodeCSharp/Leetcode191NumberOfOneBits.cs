3e88c2ce2b2f88dc04389595f22e4e30e0903807
Boris Truong <linhtch90@gmail.com>
2026-06-16 01:19:30 +0700

    [Leetcode-191] - Number of one bits- Bit - Easy

--- MyLeetcodeCSharp/Leetcode191NumberOfOneBits.cs
+++ MyLeetcodeCSharp/Leetcode191NumberOfOneBits.cs
@@ -5,7 +5,7 @@ public class Leetcode191NumberOfOneBits
     public int HammingWeight(int n)
     {
         int count = 0;
-        if (n != 0)
+        while (n != 0)
         {
             n &= n - 1;
             count++;

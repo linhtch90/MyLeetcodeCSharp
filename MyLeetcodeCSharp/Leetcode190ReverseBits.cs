af6049e19598ab4de8d69b7ae65076ca92db456a
Boris Truong <linhtch90@gmail.com>
2026-06-16 23:53:38 +0700

    [Leetcode-190] - Reverse Bits - Bits - Easy

---
+++ MyLeetcodeCSharp/Leetcode190ReverseBits.cs
@@ -0,0 +1,17 @@
+namespace MyLeetcodeCSharp;
+
+public class Leetcode190ReverseBits
+{
+    public int ReverseBits(int n)
+    {
+        int result = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            int bit = n & 1;
+            result = (result << 1) | bit;
+            n >>= 1;
+        }
+
+        return result;
+    }
+}
